d1cce4058426b05833d26b23ffd3c4eec3d7061e
Shubhi <shubhishaneshah96@gmail.com>
2023-04-06 00:44:17 +0530

    Added Test class

---
+++ src/com/shubhi/demo/Test.java
@@ -0,0 +1,21 @@
+package com.shubhi.demo;
+
+import java.util.*;
+
+public class Test {
+
+	public static void main(String[] args) {
+	  ArrayList<String> list = new ArrayList<String>();
+	  list.add("Java");
+	  list.add("Hibernate");
+	  list.add("Spring");
+	  
+	  System.out.println(list);
+	 
+	
+	 
+	 
+
+	}
+
+}
